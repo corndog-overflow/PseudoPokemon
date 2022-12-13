b92204014ea106af5a4673f5551993b998e0f501
sadman kabir <104959725+corndog-overflow@users.noreply.github.com>
2022-12-12 23:57:23 -0500

    Add files via upload

---
+++ Input_Handling.h
@@ -0,0 +1,22 @@
+#ifndef INPUT_HANDLING
+#define INPUT_HANDLING
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Invalid_Input {
+
+ public:
+
+ Invalid_Input(string in_ptr) : msg_ptr (in_ptr) { }
+
+ const string msg_ptr;
+
+ private:
+
+ Invalid_Input ();
+
+};
+
+#endif
